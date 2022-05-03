be3486682325fba48fcf46c4c10e41f6ceffbefa
윤다형 <dahyeoung.yoon@ta9.co.kr>
2022-05-03 17:34:56 +0900

    4.2.1.1 index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +29,43 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            Path file = getFile(in);
+            byte[] body = Files.readAllBytes(file);
+            // end of todo
+            
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private Path getFile(InputStream in) throws IOException {
+        InputStreamReader inputReader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(inputReader);
+        
+        String line = "start";
+        StringBuilder result = new StringBuilder();
+        while(!"".equals(line)) {
+            line = br.readLine();
+            result.append(line);
+        }
+        
+        String requestInfo = result.toString();
+        String[] token = requestInfo.split(" "); 
+        String url = token[1];
+        
+        String requestPath = url;
+        int idx = url.indexOf("?");
+        if(idx > -1) {
+        	requestPath = url.substring(0, idx);	
+        }
+        
+        // String params = url.substring(idx);
+        
+        Path file = new File("./webapp" + requestPath).toPath();
+        return file;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
